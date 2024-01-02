f3ef64d65515f32c0f56c513656125d7e65ec2bd
Hyyena <sjtxm0320@gmail.com>
2024-01-03 04:24:30 +0900

    ✨ feat: app-maple-stamp-api 애플리케이션 모듈 추가

---
+++ app-maple-stamp-api/src/test/java/com/mapletrend/AppMapleStampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapletrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMapleStampApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
