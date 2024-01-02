c410036a815afd6fdbb5c8b045ea39463140b2bd
Hyyena <sjtxm0320@gmail.com>
2024-01-02 20:41:18 +0900

    🎉 feat: 초기 설정

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    group = "com.mapletrend"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_17
+
+    configurations {
+        all {
+            // Logback 의존성 제외
+            exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+        }
+    }
+
+    dependencies {
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.register<Copy>("copySecrets") {
+        from("../secrets/")
+        into("src/main/resources/secrets")
+    }
+
+    tasks.named<ProcessResources>("processResources") {
+        dependsOn("copySecrets")
+    }
+}
+
+project("common") {
+    val jar: Jar by tasks
+    val bootJar: BootJar by tasks
+
+    bootJar.enabled = false
+    jar.enabled = true
+}
+
+project("nexon-open-api-core") {
+    dependencies {
+        implementation(project(":common"))
+
+        implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.retry:spring-retry")
+    }
+
+    val jar: Jar by tasks
+    val bootJar: BootJar by tasks
+
+    bootJar.enabled = false
+    jar.enabled = true
+}
+
+tasks.named<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Maple-Trend-BE"
+
+// 공통 모듈 계층
+include("common")
+
+// 내부 모듈 계층
+include("nexon-open-api-core")
