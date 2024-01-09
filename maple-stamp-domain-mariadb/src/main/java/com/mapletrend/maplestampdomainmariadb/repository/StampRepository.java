3be39c6a07d92addebb067212a45fdec9067dd26
Hyyena <70932170+Hyyena@users.noreply.github.com>
2024-01-10 02:38:01 +0900

    ✨ feat: 인장 검색 API 추가 (#51)

--- maple-stamp-domain-mariadb/src/main/java/com/mapletrend/maplestampdomainmariadb/repository/StampRepository.java
+++ maple-stamp-domain-mariadb/src/main/java/com/mapletrend/maplestampdomainmariadb/repository/StampRepository.java
@@ -8,6 +8,8 @@ public interface StampRepository extends JpaRepository<Stamp, Long> {
 
     Optional<Stamp> findByUuid(String uuid);
 
+    Optional<Stamp> findByInvenNickname(String invenNickname);
+
     Optional<Stamp> findByNexonApiKeyAndInvenNicknameAndCharacterName(String apiKey, String invenNickname,
                                                                       String characterName);
 }
