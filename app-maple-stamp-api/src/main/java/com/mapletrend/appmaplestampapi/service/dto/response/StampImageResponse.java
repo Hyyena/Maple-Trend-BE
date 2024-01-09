490baad3e75663cedff6560df80187aa246eeb61
Hyyena <70932170+Hyyena@users.noreply.github.com>
2024-01-10 02:38:01 +0900

    ✨ feat: 인장 검색 API 추가 (#51)

--- app-maple-stamp-api/src/main/java/com/mapletrend/appmaplestampapi/service/dto/response/StampImageResponse.java
+++ app-maple-stamp-api/src/main/java/com/mapletrend/appmaplestampapi/service/dto/response/StampImageResponse.java
@@ -1,9 +1,13 @@
 package com.mapletrend.appmaplestampapi.service.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Builder
 @Getter
-public class StampResponse {
+public class StampImageResponse {
+
+    private final byte[] stampImage;
 }
