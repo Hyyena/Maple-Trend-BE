5f8b97822af9b0d86de505f9ddedb0259fe25676
Hyyena <sjtxm0320@gmail.com>
2024-01-10 02:37:30 +0900

    ✨ feat: 인장 검색 API 추가

--- app-maple-stamp-api/src/main/java/com/mapletrend/appmaplestampapi/config/WebConfig.java
+++ app-maple-stamp-api/src/main/java/com/mapletrend/appmaplestampapi/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3003", "https://maplestory.nexon.com", "https://www.mapletrend.co.kr")
-                .allowedMethods("POST")
+                .allowedOrigins("http://localhost:3003", "https://www.mapletrend.co.kr")
+                .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
 }
